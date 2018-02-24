34b281b017c164d08dd75c97b0e4d73369c0c6a6
Jessie Trieu <Jessie Trieu>
2018-02-24 14:06:04 -0800

    Added classes to Models.py and changed some variable names in HTML Templates.

--- PCW/PCW_APP/models.py
+++ PCW/PCW_APP/models.py
@@ -8,3 +8,19 @@ class User(models.Model):
    registered = models.IntegerField(default=0)
    username = models.CharField(max_length=200)
    password = models.CharField(max_length=200)
+
+class Markers(models.Model):
+   name = models.CharField(max_length=60)
+   address = models.CharField(max_length=80)
+   lat = models.FloatField(max_digits=10, decimal_places=6)
+   lng = models.FloatField(max_digits=10, decimal_places=6)
+   type = models.CharField(max_length=30)
+
+class Events(models.Model):
+   title = models.CharField(max_length=50)
+   location = models.CharField(max_length=60)
+   day = models.CharField(max_length=10)
+   time = models.DateTimeField()
+
+class Days(models.Model);
+   name = models.CharField(max_length=10)
