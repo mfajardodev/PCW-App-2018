dd2197b1b51e4a143ea6cca23825282d57889bd1
Jessie Trieu <Jessie Trieu>
2018-04-08 09:52:16 -0700

    fixed and condensed script

--- Sheets_Java/src/main/java/Quickstart.java
+++ Sheets_Java/src/main/java/Quickstart.java
@@ -122,7 +122,6 @@ public class Quickstart {
         Connection cnc = null;
 
         try {
-            // Build a new authorized API client service.
             Sheets service = getSheetsService();
 
             /* set up data pull from google sheet ( QR Code Generator )
@@ -136,10 +135,6 @@ public class Quickstart {
                 .execute();
 
             List<List<Object>> values = response.getValues();
-          
-            // file to write to
-            File file = new File("/Users/jessietrieu/repos/PCW_APP_2018/Sheets_Java/data/Sheet1.txt");
-            PrintWriter printWriter = null;
             
             try {
                 // establish database connection and statements/prepared statements/results
@@ -150,45 +145,32 @@ public class Quickstart {
                 String st = "select id from auth_user where email = ?";
                 String update = "update PCW_APP_profile set QR_code = ?, Organization = ?, hostee = ? where user_id = ?"; //add hostee = ?
 
-                printWriter = new PrintWriter(file);
-                if (values == null || values.size() == 0) {
-                   printWriter.println("No data found.");
-                } else {
-                   printWriter.print("Email, Org, QR Code Link\n");
-                    /* 
-                       Print columns C, D and G, which correspond to indices 0, 1 and 4.
-                       Pull Initial Email and QR Code and insert into DB
-                    */ 
-                   for (List row : values) { 
-                        printWriter.printf("%s, %s\n", row.get(0), row.get(1), row.get(4));
-                        String email = row.get(0).toString();
-                        String org = row.get(1).toString();
-                        String code = row.get(4).toString();
-                        ps = cnc.prepareStatement(st);
-                        ps.setString(1,email);
-                        res1 = ps.executeQuery();
-                        while( res1.next() ) {
-                            upd = cnc.prepareStatement(update);
-                            upd.setString(1, code);
-                            upd.setString(2, org);
-                            upd.setInt(3, 1);
-                            int id = res1.getInt("id");
-                            upd.setInt(4, id);
-                            upd.executeUpdate();
-                        }
+                /* 
+                   Print columns C, D and G, which correspond to indices 0, 1 and 4.
+                   Pull Initial Email and QR Code and insert into DB
+                */ 
+               for (List row : values) { 
+                    String email = row.get(0).toString();
+                    String org = row.get(1).toString();
+                    String code = row.get(4).toString();
+                    ps = cnc.prepareStatement(st);
+                    ps.setString(1,email);
+                    res1 = ps.executeQuery();
+                    while( res1.next() ) {
+                        upd = cnc.prepareStatement(update);
+                        upd.setString(1, code);
+                        upd.setString(2, org);
+                        upd.setInt(3, 1);
+                        int id = res1.getInt("id");
+                        upd.setInt(4, id);
+                        upd.executeUpdate();
                     }
-                } 
-            }
+                }
+            } 
             catch (SQLException e) {
                 e.printStackTrace();
             }
-            catch (FileNotFoundException e){
-                e.printStackTrace();
-            }
             finally {
-                if( printWriter != null ) {
-                   printWriter.close();
-                }
                 closeEm(cnc);
             }
         }
@@ -205,116 +187,88 @@ public class Quickstart {
         catch ( ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         Connection cnc = null;
+        try{
+            cnc = DriverManager.getConnection("jdbc:mysql://wf-207-38-86-69.webfaction.com/pcw_app?serverTimezone=America/Los_Angeles", "pcw", "Pcw2018!!!!!"); //?autoReconnect=true&useSSL=false
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         try {
-            // List of emails from rows ( checkins ) that fall between start and end dates 
-            ArrayList<String> checkedRows = new ArrayList<String>();
+            while (true) {
+                ArrayList<String> checkedRows = new ArrayList<String>();
+                Sheets service = getSheetsService();
+                String spreadsheetId = "1L2fYLcD6bw5SS04khckeDATUPPqKRIJwOMEYrfQa1WU";
+                String range = "Form Responses 1!A2:E";
+                ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
+                List<List<Object>> values = response.getValues();
 
-            // Build a new authorized API client service.
-            Sheets service = getSheetsService();
+                try {
+                    
+                    PreparedStatement ps = null;
+                    ResultSet res1 = null;
+                    ResultSet res2 = null;
+                    Statement stmt = null;
+                    PreparedStatement upd = null;
+                    String st = "select * from PCW_APP_signintime";
+                    String pst = "select * from PCW_APP_profile p, auth_user au where au.id = p.user_id and ? = au.email";
+                    String update = "update PCW_APP_profile set signed_in = 1 where user_id = ?";
 
-            /* set up data pull from google sheet ( PCW2018 Form )
-             * link : https://docs.google.com/spreadsheets/d/1L2fYLcD6bw5SS04khckeDATUPPqKRIJwOMEYrfQa1WU/edit#gid=1987675620
-             */
-            String spreadsheetId = "1L2fYLcD6bw5SS04khckeDATUPPqKRIJwOMEYrfQa1WU";
-            String range = "Form Responses 1!A2:E";
-
-            ValueRange response = service.spreadsheets().values()
-                .get(spreadsheetId, range)
-                .execute();
-        
-            List<List<Object>> values = response.getValues();
-
-            // file to write to
-            File file = new File("/Users/jessietrieu/repos/PCW_APP_2018/Sheets_Java/data/Sheet2.txt");
-            PrintWriter printWriter = null;
-            //int i = 0;
-            try {
-                // establish database connection and statements/prepared statements/results
-                cnc = DriverManager.getConnection("jdbc:mysql://wf-207-38-86-69.webfaction.com/pcw_app?serverTimezone=America/Los_Angeles", "pcw", "Pcw2018!!!!!"); //?autoReconnect=true&useSSL=false
-                PreparedStatement ps = null;
-                ResultSet res1 = null;
-                ResultSet res2 = null;
-                Statement stmt = null;
-                PreparedStatement upd = null;
-                String st = "select * from PCW_APP_signintime";
-                String pst = "select * from PCW_APP_profile p, auth_user au where au.id = p.user_id and ? = au.email";
-                String update = "update PCW_APP_profile set signed_in = 1 where user_id = ?";
-
-                while(true) {//i < 2) {
-                    printWriter = new PrintWriter(file);
-                    if (values == null || values.size() == 0) {
-                       printWriter.println("No data found.");
-                    } else {
-                       printWriter.print("Formatted Timestamp, QR Code Link\n");
-
-                        /* 
-                           Filter out pulled date to match start and end time
-                           Print columns B and E, which correspond to indices 1 and 4. ( Formatted Timestamp & Email )
-                        */
-                       for (List row : values) { 
-                          stmt = cnc.createStatement();
-                          res1 = stmt.executeQuery(st);
-                          while( res1.next() ) {
+                    /* 
+                       Filter out pulled date to match start and end time
+                       Print columns B and E, which correspond to indices 1 and 4. ( Formatted Timestamp & Email )
+                    */
+                    for (List row : values) { 
+                        stmt = cnc.createStatement();
+                        res1 = stmt.executeQuery(st);
+                        while( res1.next() ) {
                             java.util.Calendar cal = Calendar.getInstance();
                             cal.setTimeZone(TimeZone.getTimeZone("PST"));
-                        
                             Calendar calendar = Calendar.getInstance();
                             calendar.setTime(new Date());
                             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                             sdf.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
-
                             Timestamp start = Timestamp.valueOf(sdf.format(res1.getTimestamp("startTime")));
                             Timestamp end = Timestamp.valueOf(sdf.format(res1.getTimestamp("endTime")));
                             Timestamp dt = Timestamp.valueOf(row.get(1).toString());
-            
+
                             if( dt.before(end) && dt.after(start) ) {
                                 checkedRows.add(row.get(4).toString());
-                                System.out.println("First filter");
-                                printWriter.printf("%s, %s\n", row.get(1), row.get(4));
                             }
-                          }
-                       }
-
-                       // filter 2: sets signedIn to 1
-                       for(String row : checkedRows) {
-                            String email = row;
-                            ps = cnc.prepareStatement(pst);
-                            ps.setString(1,email);
-                            res2 = ps.executeQuery();
-                            while( res2.next() ) {
-                                int user = res2.getInt("user_id");
-                                upd = cnc.prepareStatement(update);
-                                upd.setInt(1, user);
-                                upd.executeUpdate();
-                            }
-                       }
+                        }
                     }
-                    // 2 second delay
-                    Thread.sleep(2000);
-                    System.out.println("Testing..." + new Date());
-                    //i++;
+
+                    // filter 2: sets signedIn to 1
+                    for(String row : checkedRows) {
+                        String email = row;
+                        ps = cnc.prepareStatement(pst);
+                        ps.setString(1,email);
+                        res2 = ps.executeQuery();
+                        while( res2.next() ) {
+                            int user = res2.getInt("user_id");
+                            upd = cnc.prepareStatement(update);
+                            upd.setInt(1, user);
+                            upd.executeUpdate();
+                        }
+                    }
                 }
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
-            catch (FileNotFoundException e){
-                e.printStackTrace();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            finally {
-                if( printWriter != null ) {
-                   printWriter.close();
+                catch (SQLException e) {
+                    e.printStackTrace();
                 }
-                closeEm(cnc);
+                Thread.sleep(2000); // 2 second delay
+                System.out.println("Pulled..." + new Date());
             }
         }
         catch ( IOException e ) {
             e.printStackTrace();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            closeEm(cnc);
         }
     }
 
